51657ce834b3ef15f2bb05f5c907791bfd6aa27c
Harsh Kumar <130214564+harsh031104@users.noreply.github.com>
2024-02-19 16:01:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+from flask_login import LoginManager , UserMixin , login_user , login_required , logout_user , current_user
+from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
+app.secret_key = "49494asdklfjasdkflaskdf"
+
+app.config['MYSQL_HOST'] ="localhost"
+app.config['MYSQL_USER'] ="root"
+app.config['MYSQL_PASSWORD'] ="harsh2004"
+app.config['MYSQL_DB'] ="student_info"
+
+mysql = MySQL(app)
+login_manage = LoginManager()
+login_manage.init_app(app)
+bcrypt = Bcrypt(app)
+
+@login_manage.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+class User(UserMixin):
+    def __init__(self , user_id , name , rollNo , semester ,email ):
+        self.id = user_id
+        self.name = name
+        self.rollNo = rollNo
+        self.semester = semester
+        self.email = email
+
+    @staticmethod
+    def get(user_id):
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT name , semester , rollNo , email from users where id = %s', (user_id,))
+        result = cursor.fetchone()
+        cursor.close()
+        if result: 
+            User(user_id , result[0] , result[1] , result[2] , result[3])
+
+@app.route('/')
+def index():
+    return 'Home page'
+
+@app.route('/login' , methods = ['GET' , 'POST'])
+def login():
+    if request.method == 'POST':
+        #name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT id ,name ,rollNo , semester , email, password from users where email = %s',(email,) )
+        user_data = cursor.fetchone()
+        cursor.close()
+
+        if user_data and bcrypt.check_password_hash(user_data[5] , password):
+            user = User(user_data[0] , user_data[1] , user_data[2] , user_data[3] , user_data[4])
+            login_user(user)
+            return redirect(url_for('dashboard'))
+
+       
+    return render_template('login.html')
+
+@app.route('/registration' , methods = ['GET' , 'POST'])
+def registration():
+    if request.method == 'POST':
+        name = request.form['name']
+        semester = request.form['semester']
+        rollNo = request.form['rollNo']
+        email = request.form['email']
+        password = request.form['password']
+
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO users (name , rollNo, semester ,password , email) values(%s,%s,%s,%s,%s)' , (name , rollNo , semester , hashed_password , email))
+        mysql.connection.commit()
+        cursor.close()
+        return redirect(url_for('login'))
+        
+    return render_template('registration.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
